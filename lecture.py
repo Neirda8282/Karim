e14da9293435a40c3f5c656f12dca2ae247ec148
Neirda <adrien8282@hotmail.fr>
2018-06-05 21:54:44 +0200

    test

---
+++ lecture.py
@@ -0,0 +1,26 @@
+# (C) Fabrice Sincère
+import wave
+import binascii
+
+NomFichier = input('Entrer le nom du fichier : ')
+Monson = wave.open(NomFichier,'r')	# instanciation de l'objet Monson
+
+print("\nNombre de canaux :",Monson.getnchannels())
+print("Taille d'un échantillon (en octets):",Monson.getsampwidth())
+print("Fréquence d'échantillonnage (en Hz):",Monson.getframerate())
+print("Nombre d'échantillons :",Monson.getnframes())
+print("Type de compression :",Monson.getcompname())
+
+TailleData = Monson.getnchannels()*Monson.getsampwidth()*Monson.getnframes()
+
+print("Taille du fichier (en octets) :",TailleData + 44)
+print("Nombre d'octets de données :",TailleData)
+
+
+Monson.setpos(echDebut)
+T=[]
+for i in range(0,Monson.getnframes):
+    T.append((Monson.tell(),'\t\t',binascii.hexlify(Monson.readframes(1))))
+
+Monson.close()
+print(T)
